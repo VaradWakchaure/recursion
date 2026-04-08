b8872c09e795deae0c53ac95776cc836465891fa
VaradWakchaure <varadrw2006@gmail.com>
2026-04-08 13:20:41 +0530

    Upgraded build.sh to create a django superuser if it doesn't exist

--- build.sh
+++ build.sh
@@ -3,4 +3,13 @@ set -o errexit
 
 pip install -r requirements.txt
 python manage.py migrate
+
+# Create superuser automatically
+python manage.py shell << END
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username="admin").exists():
+    User.objects.create_superuser("admin", "admin@example.com", "admin123")
+END
+
 python manage.py collectstatic --noinput
