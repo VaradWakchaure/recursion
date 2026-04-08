4cf0e43834df1fb97c0c2fc06e4a0496f7e46cf7
VaradWakchaure <varadrw2006@gmail.com>
2026-04-08 21:22:27 +0530

    modifications to build.sh

--- build.sh
+++ build.sh
@@ -5,11 +5,11 @@ pip install -r requirements.txt
 python manage.py migrate
 
 # Create superuser automatically
-python manage.py shell << END
+python manage.py shell -c "
 from django.contrib.auth import get_user_model
 User = get_user_model()
-if not User.objects.filter(username="admin").exists():
-    User.objects.create_superuser("admin", "admin@example.com", "admin123")
-END
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@example.com', 'admin123')
+"
 
 python manage.py collectstatic --noinput
